bf928ab4f1f8100feeea10972c8905014c2afbba
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-26 16:17:27 -0300

    👨🏻‍💻 feat: finishing the construction of the Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,11 @@
+import './styles.scss'
+
 export const Header = () => {
   return (
-    <></>
+    <header>
+      <span>video streaming</span>
+      <div className="divider" />
+      <p>platform</p>
+    </header>
   )
 }
