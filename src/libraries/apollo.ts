8b90c36308ae35740433ae0926b5871130f3c48e
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-26 14:00:23 -0300

    ➕ title: importing API file and wrapping App component in ApolloProvider

--- src/libraries/apollo.ts
+++ src/libraries/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4vfksih1hgt01uoe7gt6lvf/master',
   cache: new InMemoryCache()
 })
