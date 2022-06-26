f4243a4bd1314f5ada5e2fc8690c4218856bf760
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-26 16:44:07 -0300

    ➕ title: importing Main component to Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/header"
+import { Main } from "../components/main"
 
 export const Home = () => {
   return (
     <div className="container-home">
       <Header />
+      <Main />
     </div>
   )
 }
