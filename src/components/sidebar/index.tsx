ccdc3eda8e153fe0249652b5e45807052aa809a0
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-26 19:52:01 -0300

    👨🏻‍💻 feat: calling the API and passing the events to the Lesson component by props

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,48 @@
+import { gql, useQuery } from '@apollo/client'
+import { Lesson } from '../lesson'
+import './styles.scss'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      title
+      slug
+      availableAt
+      lessonType
+    }
+  }
+`
+
+type GetLessonsQueryResponse = {
+  lessons: {
+    id: string
+    title: string
+    slug: string
+    availableAt: string
+    lessonType: 'live' | 'class'
+  }[]
+}
+
 export const Sidebar = () => {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
   return (
     <aside>
-      Sidebar
+      <span className="sidebar-title">
+        Cronograma das aulas
+      </span>
+      <div className="container-lessons">
+        {data?.lessons.map(lesson => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            availableAt={new Date(lesson.availableAt)}
+            type={lesson.lessonType}
+          />
+        ))}
+      </div>
     </aside>
   )
 }
