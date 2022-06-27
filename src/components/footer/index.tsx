876a18578f4949e37ac69884cf78cba23aaad741
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-06-27 20:42:18 -0300

    👨🏻‍💻 feat: structuring the Footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,19 @@
+import { Rocket } from 'phosphor-react'
 import './styles.scss'
 
 export const Footer = () => {
+  const CURRENT_YEAR = new Date().getFullYear()
+
   return (
     <footer>
-      Footer
+      <div className="content-logo-copy">
+        <div className="footer-logo">
+          <Rocket size={30} />
+          <span>jgalves</span>
+        </div>
+        <small>&copy;{CURRENT_YEAR} JGAlves - Todos os direitos reservados</small>
+      </div>
+      <p>Política de privacidade</p>
     </footer>
   )
 }
